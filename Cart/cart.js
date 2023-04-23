39819663d6d39b4c330410edbc85966ed7722b12
gwanseeum <9v.veu.v@gmail.com>
2023-04-23 18:42:44 +0900

    Design: Header와 Footer 추가, 페이지 추가

--- Cart/cart.js
+++ Cart/cart.js
@@ -1,5 +1,9 @@
+import { createHeader } from '/src/Default-Layout/header.js';
+import { createFooter } from '/src/Default-Layout/footer.js';
 import { cartLoad } from './cartModules/cartLoad.js';
 
 window.onload = () => {
+  createHeader();
+  createFooter();
   cartLoad();
 };
