f75634047dc86457dfe50207228f78b824d3586e
gwanseeum <9v.veu.v@gmail.com>
2023-04-24 23:09:58 +0900

    Add: 주문결제 모듈 추가

---
+++ src/pages/order/order.js
@@ -0,0 +1,18 @@
+import { createHeader } from '/src/default_layout/header.js';
+import { createFooter } from '/src/default_layout/footer.js';
+import { execDaumPostcode } from '/src/modules/orderModules/inputUserAddress.js';
+import { orderListTemplate } from '/src/modules/orderModules/userOrderList.js';
+import { priceSetting } from '/src/modules/orderModules/goodsTotalPrice.js';
+import { sendReceiverProfile } from '/src/modules/orderModules/userShippingInput.js';
+
+document.addEventListener('DOMContentLoaded', createHeader);
+document.addEventListener('DOMContentLoaded', createFooter);
+document.addEventListener('DOMContentLoaded', orderListTemplate);
+document.addEventListener('DOMContentLoaded', priceSetting);
+
+document
+  .getElementById('getuserAddress')
+  .addEventListener('click', execDaumPostcode);
+document
+  .getElementById('orderComplete')
+  .addEventListener('click', sendReceiverProfile);
