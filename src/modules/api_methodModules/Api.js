9ac1ca23308fb2297d13175e275e1f5cf7139082
JuYoung Lee <lee_ju_0@naver.com>
2023-04-25 22:50:54 +0900

    Add: API Fetcher 추가

--- src/modules/api_methodModules/Api.js
+++ src/modules/api_methodModules/Api.js
@@ -1,3 +1,63 @@
+async function request({ endpoint, method, params = '', data = {} }) {
+  const apiUrl = params ? `${endpoint}/${params}` : endpoint;
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+  };
+
+  try {
+    let response;
+    if (method === 'GET') {
+      response = await axios.get(apiUrl, { headers });
+    } else if (method === 'POST') {
+      response = await axios.post(apiUrl, data, { headers });
+    } else if (method === 'PATCH') {
+      response = await axios.patch(apiUrl, data, { headers });
+    } else if (method === 'PUT') {
+      response = await axios.put(apiUrl, data, { headers });
+    } else if (method === 'DELETE') {
+      response = await axios.delete(apiUrl, { headers, data });
+    } else {
+      throw new Error('잘못된 메소드 접근입니다.');
+    }
+
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      const { reason } = error.response.data;
+      throw new Error(reason);
+    } else {
+      throw new Error('Failed to make request');
+    }
+  }
+}
+
+async function get(endpoint, params = '') {
+  return await request({ endpoint, method: 'GET', params });
+}
+
+async function post(endpoint, data) {
+  return await request({ endpoint, method: 'POST', data });
+}
+
+async function put(endpoint, params = '', data) {
+  return await request({ endpoint, method: 'PUT', params, data });
+}
+
+async function patch(endpoint, params = '', data) {
+  return await request({ endpoint, method: 'PATCH', params, data });
+}
+
+async function del(endpoint, params = '', data = {}) {
+  return await request({ endpoint, method: 'DELETE', params, data });
+}
+
+export { get, post, put, patch, del as delete };
+
+/*
+export const DUMMY_URL = '/src/dummyProducts.json';
+export const ALL_PRODUCT_URL = 'http://34.64.112.254:5000/api/products';
+
 async function getApi(url, params = {}, headers = {}) {
   try {
     const response = await axios.get(url, {
@@ -42,3 +102,4 @@ async function deleteApi(url, params) {
 }
 
 export { getApi, postApi, putApi, deleteApi };
+*/
