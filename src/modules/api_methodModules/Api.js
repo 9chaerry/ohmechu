2a6dff18bfcce3a858678bf23176f2adc4bb8116
JungHyun <wjstkwhwjdgus@gmail.com>
2023-04-26 10:30:46 +0900

    Fix: API - fetch를 axios로 변경

--- src/modules/api_methodModules/Api.js
+++ src/modules/api_methodModules/Api.js
@@ -1,44 +1,49 @@
-async function getApi(url, params = {}, headers = {}) {
+async function request({ endpoint, method, params = '', data = {} }) {
+  const apiUrl = params ? `${endpoint}/${params}` : endpoint;
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+  };
+
   try {
-    const response = await axios.get(url, {
-      params,
-      headers,
-    });
+    let response;
+    if (method === 'GET') {
+      response = await axios.get(apiUrl, { headers });
+    } else if (method === 'POST') {
+      response = await axios.post(apiUrl, data, { headers });
+    } else if (method === 'PATCH') {
+      response = await axios.patch(apiUrl, data, { headers });
+    } else if (method === 'DELETE') {
+      response = await axios.delete(apiUrl, { headers, data });
+    } else {
+      throw new Error('잘못된 메소드 접근입니다.');
+    }
+
     return response.data;
   } catch (error) {
-    console.error(`API GET 요청 실패: ${error}`);
-    throw error;
+    if (error.response) {
+      const { reason } = error.response.data;
+      throw new Error(reason);
+    } else {
+      throw new Error('Failed to make request');
+    }
   }
 }
 
-async function postApi(url, data) {
-  try {
-    const response = await axios.post(url, data);
-    return response.data;
-  } catch (error) {
-    console.error(`API POST 요청 실패: ${error}`);
-    throw error;
-  }
+async function get(endpoint, params = '') {
+  return await request({ endpoint, method: 'GET', params });
 }
 
-async function putApi(url, data) {
-  try {
-    const response = await axios.put(url, data);
-    return response.data;
-  } catch (error) {
-    console.error(`API PUT 요청 실패: ${error}`);
-    throw error;
-  }
+async function post(endpoint, data) {
+  return await request({ endpoint, method: 'POST', data });
 }
 
-async function deleteApi(url, params) {
-  try {
-    const response = await axios.delete(url, { params });
-    return response.data;
-  } catch (error) {
-    console.error(`API DELETE 요청 실패: ${error}`);
-    throw error;
-  }
+async function patch(endpoint, params = '', data) {
+  return await request({ endpoint, method: 'PATCH', params, data });
 }
 
-export { getApi, postApi, putApi, deleteApi };
+async function del(endpoint, params = '', data = {}) {
+  return await request({ endpoint, method: 'DELETE', params, data });
+}
+
+export { get, post, patch, del as delete };
