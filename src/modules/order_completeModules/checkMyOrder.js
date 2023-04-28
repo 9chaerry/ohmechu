f0f9ee1c2e5f17e85fee9ef3d5f35d0e92102667
JungHyun <wjstkwhwjdgus@gmail.com>
2023-04-29 03:25:19 +0900

    Fix: 비회원일 경우 주문완료 -> 주문확인 시 직전 주문조회 안되는 기능 수정

--- src/modules/order_completeModules/checkMyOrder.js
+++ src/modules/order_completeModules/checkMyOrder.js
@@ -1,10 +1,15 @@
+import { getOrderedId } from '/src/modules/order_completeModules/getOrderId.js';
+
 function checkOrder(e) {
   e.stopPropagation();
+
+  const getOrderId = getOrderedId();
+
   const isLoggedIn = false;
   if (isLoggedIn) {
     window.location.href = '/src/pages/order_list/order_list.html';
   } else {
-    window.location.href = '/src/pages/login/login.html';
+    window.location.href = `/src/pages/order_list/order_list.html?order=${getOrderId}`;
   }
 }
 
