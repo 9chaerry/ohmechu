4d4267480745f872cd8e60e19c547321b03ff794
JungHyun <wjstkwhwjdgus@gmail.com>
2023-04-28 17:18:17 +0900

    Fix: 주문 내역 템플릿 이미지 눌림 수정 & 레이아웃 수정

--- src/modules/order_editModules/orderedListTemplate.js
+++ src/modules/order_editModules/orderedListTemplate.js
@@ -1,11 +1,14 @@
 function orderedListTemplate(data, amountData) {
   const orderedGoodsList = document.createElement('tr');
-  orderedGoodsList.classList.add = 'flex justify-between';
+  orderedGoodsList.classList.add('flex', 'justify-between');
+
   orderedGoodsList.innerHTML = `
-    <td class="flex-shrink-0 w-[25%]">
-          <img src="${data.img}" class="w-[150px] h-[150px]" />
+    <td class="flex flex-shrink-0 w-[25%]">
+          <img src="${
+            data.img
+          }" class="w-[150px] h-[150px] object-cover object-center" />
         </td>
-        <td class="flex-col flex-grow">
+        <td class="flex flex-col justify-center flex-grow w-[30%]">
           <span class="block text-left text-[17px] font-medium">${
             data.name
           }</span>
@@ -13,14 +16,14 @@ function orderedListTemplate(data, amountData) {
             ' / '
           )}</span>
         </td>
-        <td class="flex-shrink-0 w-[50%]">
+        <td class="flex flex-shrink-0 items-center w-[10%] justify-end">
           <span class="text-xl font-semibold">${amountData.amount}</span>
           <span class="text-xl font-semibold ml-1"> 개</span>
         </td>
-        <td class="flex-shrink-0 w-[50%]">
-          <span class="text-xl font-semibold w-[50%]">${
+        <td class="flex flex-shrink-0 items-center w-[30%] justify-end">
+          <span class="flex text-xl font-semibold w-[50%] justify-end">${(
             data.price * amountData.amount
-          }</span>
+          ).toLocaleString('ko-KR')}</span>
           <span class="text-xl font-semibold mx-2"> 원</span>
         </td>`;
   return orderedGoodsList;
